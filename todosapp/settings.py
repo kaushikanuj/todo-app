ee6a1110dd5e2dcf0983282a6ae2414d2fcd24bd
anuj kaushik <anujkaushik380@gmail.com>
2020-12-02 19:58:36 -0800

    fout commit

--- todosapp/settings.py
+++ todosapp/settings.py
@@ -22,9 +22,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'gkbmm9q_a@h(zc5ffs)-#106nj29)czo!v4(o+i_fy^eed9@dk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 # Application definition
 
